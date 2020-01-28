8dd541613d92d2e0b447e65d5ee0143b018d178b
pramodibs <pramodnetgenius@gmail.com>
2020-01-28 21:19:54 +0530

    update postgres

--- Ubuntu-18.04/postgres_install.sh
+++ Ubuntu-18.04/postgres_install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :     Pramod Prajapati
+######################################################
+#Download and Install postgre sql database and phpPgAdmin on ubuntu 18.04 LTS
 
-to disable extra security while logging in postgress use below file and disable extra login security
+sudo apt-get install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib -y
+sudo bash 
+echo "########### Set password for postgress user:"
+sudo -u postgres psql
+ALTER USER postgres PASSWORD 'welcome@123';
+\q
+#CREATE ROLE username WITH LOGIN CREATEDB ENCRYPTED PASSWORD 'password';
+sudo apt-get install php-pgsql phppgadmin -y
+#echo '$conf['extra_login_security'] = false;' >> /etc/phppgadmin/config.inc.php
+sudo sed -i '93 s/^/#/' /etc/phppgadmin/config.inc.php
+sudo sed -i '11 s/^/#/' /etc/apache2/conf-enabled/phppgadmin.conf
+sudo service apache2 restart
+sudo service postgresql restart
 
-$conf['extra_login_security'] = false;
-$conf['extra_login_security'] = true;
+
