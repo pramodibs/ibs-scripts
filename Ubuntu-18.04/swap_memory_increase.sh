6ba196a309546457bcef8033888369a864fcb2ac
pramodibs <pramodnetgenius@gmail.com>
2020-02-25 12:41:08 +0530

    swap memory

--- Ubuntu-18.04/swap_memory_increase.sh
+++ Ubuntu-18.04/swap_memory_increase.sh
@@ -16,5 +16,4 @@ sudo cp /etc/fstab /etc/fstab.bak
 echo '/swap none swap sw 0 0' | sudo tee -a /etc/fstab
 sudo sysctl vm.swappiness=10
 sudo sysctl vm.vfs_cache_pressure=50
-
-### 
+###\n Swap Memory has been successfully updated\n###
