a44b6214ca98d1effd32c36acdfee847fcbdcd67
pramodibs <pramodnetgenius@gmail.com>
2020-02-25 12:20:20 +0530

    swap memory

---
+++ Ubuntu-18.04/swap_memory_increase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :     Pramod Prajapati
+# Date		:     25-Feb-2020
+######################################################
+# It will increase the swap memory to 16GB on Ubuntu Version
+
