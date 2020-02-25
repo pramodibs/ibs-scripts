02d6ac9d3cbd236a4b7a4f4f72bca689147b8cc2
pramodibs <pramodnetgenius@gmail.com>
2020-02-25 14:27:13 +0530

    swap memory update

--- Ubuntu-18.04/swap_memory_increase.sh
+++ Ubuntu-18.04/swap_memory_increase.sh
@@ -7,6 +7,7 @@
 # It will increase the swap memory to 16GB on Ubuntu Version
 sudo swapoff -a
 sudo fallocate -l 16G /swap
+sudo chmod 0600 /swap
 sudo mkswap /swap
 sudo swapon /swap
 #to varify run sudo swapon --show and free -h or swapon -s or grep SwapTotal /proc/meminfo
@@ -14,7 +15,9 @@ sudo swapon /swap
 #before making any changes please make a backup file
 sudo cp /etc/fstab /etc/fstab.bak
 echo '/swap none swap sw 0 0' | sudo tee -a /etc/fstab
-sudo sysctl vm.swappiness=10
-sudo sysctl vm.vfs_cache_pressure=50
-###\n Swap Memory has been successfully updated\n###
-
+#sudo sysctl vm.swappiness=10  ## for  using swap in percentage
+#sudo sysctl vm.vfs_cache_pressure=50 ## to increase the cache memory 
+#As it is currently configured, our system removes inode information from the cache too quickly.
+# We can set this to a more conservative setting like 50 by typing:
+#for permanent append file /etc/sysctl.conf with above too entry
+echo "###\n Swap Memory has been successfully updated\n###"
