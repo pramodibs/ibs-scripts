cba7137d12a9941256cc64ab145f139b7db7574b
pramod prajapati <pramodnetgenius@gmail.com>
2020-04-29 00:02:52 +0530

    vlc media player

---
+++ Ubuntu-18.04/vlc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########################################################
+# Written to be used on 64 bits computers
+# Written By    :       Pramod Prajapati
+# Date		:	28.04.2020
+########################################################
+# Downloading & Installing VLC for Ubuntu Version
+sudo apt-get update -y 
+sudo snap install vlc
+echo "################ VLC Installation Sucessfully ######################"
+
