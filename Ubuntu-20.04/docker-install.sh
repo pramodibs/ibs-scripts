fda6c4e0f43a8799f0355be7937e732a7b9a8bfb
pramod prajapati <pramodnetgenius@gmail.com>
2021-01-19 22:23:28 +0530

    docker install

---
+++ Ubuntu-20.04/docker-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#############################
+#
+# Author : Pramod Prajapati
+# Dt. : 19.01.2021
+#
+#############################
+#Installing docker on Ubuntu 20.04 64Bit OS
+
+sudo apt-get update
+sudo apt install 
